824d58c419fde32818f79161a5a5c30d5e449711
Mahdi_Murshed <123mahdi12345@gmail.com>
2022-08-17 23:16:06 +0600

    build: initializing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  extends: ["next", "next/core-web-vitals", "eslint:recommended"],
+  globals: {
+    React: "readonly",
+  },
+  rules: {
+    "no-unused-vars": [1, { args: "after-used", argsIgnorePattern: "^_" }],
+  },
   settings: {
     next: {
       rootDir: ["apps/*/"],
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,9 @@
-import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
+import { Button } from '@mantine/core';
+
+import React from 'react';
+
+const CustomButton = () => {
+  return <Button>New button </Button>;
 };
+
+export default CustomButton;
